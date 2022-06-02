e021d3b143e07440a95e1129a2c3094a8bb43dd0
Wesley <wesleyinsley.payments@gmail.com>
2022-06-02 17:39:01 -0400

    main-commit

---
+++ js/menu-change.js
@@ -0,0 +1,41 @@
+function display1() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8850.jpg'
+}
+
+function display2() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8851.jpg'
+}
+
+function display3() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8852.jpg'
+}
+
+function display4() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8853.jpg'
+}
+
+function display5() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8854.jpg'
+}
+
+function display6() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8855.jpg'
+}
+
+function display7() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8856.jpg'
+}
+
+function display8() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8857.jpg'
+}
+
+function display9() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8858.jpg'
+}
+
+function display10() {
+    document.getElementById("menu-id").src='http://granmayamexgrill.com/images/menu/IMG_8859.jpg'
+}
+
+
